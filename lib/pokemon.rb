5992c154362ff3beea0af924e1ff88aa646c91ae
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-10 18:39:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,5 +16,7 @@ class Pokemon
     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
+
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: database_connection)
   end
 end
